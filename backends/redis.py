6430bcda360ffa921ef19e76090ee83d7a464067
Lie Ryan <lie.1296@gmail.com>
2018-08-06 03:18:15 +1000

    Add "Caveat" section about Redis

--- backends/redis.py
+++ backends/redis.py
@@ -1,5 +1,24 @@
 """
 Redis-backed queue.
+
+Caveats and missing features
+
+The current implementation is not safe for concurrent access. The rate limiter
+code will need to run this in a MULTI transaction and add some WATCH to
+correctly handle concurrent hits, essentially acquiring an exclusive write lock
+for the uid.
+
+An alternative implementation which may be more efficient in a distributed
+system is to relax the rate limiting mechanism and allow approximate rate limit
+where a few extra requests may be accepted but allows the application to handle
+rate limiting without consulting a central Redis server all the time. Instead
+the application should periodically retrieve a blocked uid list that are cached
+and consulted locally and submit the hit request asynchronously or in a
+separate thread so it does not block main request processing.
+
+For production readiness, the backend should EXPIRE the keys so that Redis will
+delete old keys that are no longer relevant. Currently, Redis will keep all
+data forever so the Redis disk space will grow indefinitely.
 """
 
 
